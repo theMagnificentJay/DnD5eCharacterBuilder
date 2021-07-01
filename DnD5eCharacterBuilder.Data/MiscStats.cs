e4ea85f063b4d147edf529ac79f08f8545d959fe
theMagnificentJay <reedjoshua408@gmail.com>
2021-07-01 07:42:08 -0500

    Added ExpertSkills.cs, MiscStats.cs, and Skills.cs. Refactored Skills.cs to be bools rather than enums and added ExpertSkills as bools to track expertise.

---
+++ DnD5eCharacterBuilder.Data/MiscStats.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD5eCharacterBuilder.Data
+{
+    public class MiscStats
+    {
+        [Key]
+        public int Id { get; set; }
+        public int SpellCastingMod { get; set; }
+        public int DexToHitMod { get; set; }
+        public int StrToHitMod { get; set; }
+        public int PassiveWisdom { get; set; }
+        public string Proficiences { get; set; }
+        [Required(ErrorMessage = "Must have at least 1 valid language (Typically this language is 'common').")]
+        public string Languages { get; set; }
+        public int Initiative { get; set; }
+        [Required(ErrorMessage = "Must have valid hit points.")]
+        public int HitPoints { get; set; }
+        [Required(ErrorMessage = "Must have valid hit dice (e.g 1d10).")]
+        public string HitDice { get; set; }
+    }
+}
---
+++ DnD5eCharacterBuilder.Data/Skills.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnD5eCharacterBuilder.Data
+{
+    public class Skills
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool Acrobatics { get; set; }
+        public bool AnimalHandling { get; set; }
+        public bool Arcana { get; set; }
+        public bool Athletics { get; set; }
+        public bool Deception { get; set; }
+        public bool History { get; set; }
+        public bool Insight { get; set; }
+        public bool Intimidation { get; set; }
+        public bool Investigation { get; set; }
+        public bool Medicine { get; set; }
+        public bool Nature { get; set; }
+        public bool Perception { get; set; }
+        public bool Performance { get; set; }
+        public bool Persuasion { get; set; }
+        public bool Religion { get; set; }
+        public bool SleightOfHand { get; set; }
+        public bool Stealth { get; set; }
+        public bool Survival { get; set; }
+    }
+}
