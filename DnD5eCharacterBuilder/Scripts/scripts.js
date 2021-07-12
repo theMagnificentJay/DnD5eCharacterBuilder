77cc09138ed29365970ef0893f2c3320fce3ea7b
theMagnificentJay <reedjoshua408@gmail.com>
2021-07-11 21:45:50 -0500

    ui changes and styles added js for random quote on home page

--- DnD5eCharacterBuilder/Scripts/scripts.js
+++ DnD5eCharacterBuilder/Scripts/scripts.js
@@ -4,3 +4,13 @@
         loader.className += " hidden"; // class "loader hidden"
     });
 }, 2500);
+
+randQuote = function () {
+    var q = new Array();
+    q[0] = "q0";
+    q[1] = "q1";
+    q[2] = "q2";
+    var rand = Math.floor(Math.random() * q.length);
+    document.write(q[rand]);
+}
+randQuote();
